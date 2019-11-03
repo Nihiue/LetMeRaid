afddca1320aa31812704497f94aff8eb34e1ed6b
WangLei <nihiue@kesci.com>
2019-11-04 03:36:02 +0800

    init

---
+++ LetMeRaid/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace LetMeRaid
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 18);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(409, 215);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "配置";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Location = new System.Drawing.Point(103, 155);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(60, 29);
+            this.numericUpDown2.TabIndex = 6;
+            this.numericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 157);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = ":";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(23, 155);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(60, 29);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            17,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 125);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "启动时间";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(23, 82);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(116, 27);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "仅定时启动";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(23, 46);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(190, 27);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "定时启动 + 掉线重启";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 253);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "启用";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(11, 321);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.groupBox2.Size = new System.Drawing.Size(413, 221);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "日志";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox1.Location = new System.Drawing.Point(4, 28);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(405, 184);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(159, 253);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 46);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "停止";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button3.Location = new System.Drawing.Point(300, 253);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 46);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "说明";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(437, 552);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 10F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Let Me Raid 0.0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ LetMeRaid/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Drawing;
+
+namespace LetMeRaid
+{
+    public partial class MainWindow : Form
+
+    {
+        private System.Timers.Timer tickTimer;
+        private bool enableAutoRestart = true;
+        delegate void deleAppendLog(string text);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        [DllImport("user32.dll ")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+
+        public static Bitmap getScreen()
+        {
+            Bitmap baseImage = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics g = Graphics.FromImage(baseImage);
+            g.CopyFromScreen(new Point(0, 0), new Point(0, 0), Screen.AllScreens[0].Bounds.Size);
+            g.Dispose();
+            return baseImage;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Timers.Timer timer = new System.Timers.Timer(5000);
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.onTick);
+            timer.AutoReset = true;
+            timer.Enabled = false;
+            this.tickTimer = timer;
+        }
+
+        public void onTick(object source, System.Timers.ElapsedEventArgs e) {
+            DateTime dt = DateTime.Now;
+            bool inTimeRange = dt.TimeOfDay.TotalMinutes >= (double)(this.numericUpDown1.Value * 60 + this.numericUpDown2.Value);
+
+            if (!inTimeRange) {
+                return;
+            }
+
+            bool[] psStatus = this.checkProcess();
+
+            if (!psStatus[0]) {
+                this.BeginInvoke(new deleAppendLog(appendLog), "未检测到战网客户端");
+                return;
+            }
+            if (psStatus[1])
+            {
+                int status = this.getWowStatus();
+
+                if (status == -1 && this.enableAutoRestart) {                    
+                    this.BeginInvoke(new deleAppendLog(appendLog), "检测到掉线，关闭魔兽世界");
+                    this.killWow();
+                } else if (status == 1) {
+                    this.BeginInvoke(new deleAppendLog(appendLog), "检测到人物选择界面，选择人物");
+                    Thread thread = new Thread(new ThreadStart(enterGame));
+                    thread.IsBackground = true;
+                    thread.Start();
+                }
+            }
+            else {
+                this.BeginInvoke(new deleAppendLog(appendLog), "启动魔兽世界");
+                Thread thread = new Thread(new ThreadStart(launchWow));
+                thread.IsBackground = true;
+                thread.Start();
+            }
+        }
+
+        /*
+            -1: 被踢
+            0: 正常
+            1: 角色选择
+        */
+
+        private int calColorErr(Color c1, Color c2)
+        {
+            return (c1.R - c2.R) * (c1.R - c2.R) + (c1.G - c2.G) * (c1.G - c2.G) + (c1.B - c2.B) * (c1.B - c2.B);
+        }
+
+        private long getDeltaE(LockBitmap lockbmp, int[,] pts) {
+
+            Color st = Color.FromArgb(123, 9, 6);
+            long e = 0;
+
+            int width = lockbmp.Width;
+            int height = lockbmp.Height;
+
+            for (int i = 0; i < 2; i++) {
+                int ptx = pts[i,0] * width / 2560;
+                int pty = pts[i,1] * height / 1440;
+                if (e > 1000000) {
+                    break;
+                }
+                for (int j = -2; j < 3; j++) {
+                    e += calColorErr(st, lockbmp.GetPixel(ptx + j, pty + j));
+                }
+            }
+
+            return e;
+        }
+        private int getWowStatus() {
+            int[,] loginPts = {
+                { 70, 1035 }, 
+                { 235, 1034 },
+                { 77, 1106 },
+                { 235, 1106 },
+                { 75, 1176 },
+                { 237, 1176 },
+                { 1211, 1013 },
+                { 1381, 1013 },
+                { 2332, 990 },
+                { 2327, 1057 },
+                { 2327, 1125 },
+                { 2337, 1345 }
+            };
+
+            int[,] choosePts = {
+                { 118, 1355 },
+                { 262, 1355 },
+                { 260, 1358 },
+                { 267, 1407 },
+                { 1155, 1323 },
+                { 1396, 1321 },
+                { 2219, 97 },
+                { 2188, 1155 },
+                { 2070, 1357 },
+                { 2238, 1356 },
+                { 2362, 1358 },
+                { 2462, 1358 }
+            };
+
+            if (this.activateWindow("魔兽世界"))
+            {
+               Bitmap bmp = getScreen();
+               LockBitmap lockbmp = new LockBitmap(bmp);
+               lockbmp.LockBits();
+
+               if (getDeltaE(lockbmp, loginPts) < 10000)
+               {
+                   return -1;
+               }
+               if (getDeltaE(lockbmp, choosePts) < 10000)
+               {
+                   return 1;
+               }
+            }
+            else
+            {
+                Console.WriteLine("WOW NOT FOUND");          
+            }
+            return 0;
+        }
+
+        private void killWow() {
+            foreach (Process p in Process.GetProcesses())
+            {
+                if (p.ProcessName.ToLower() == "wow")
+                {
+                    try
+                    {
+                        p.Kill();
+                        p.WaitForExit();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message.ToString());
+                    }
+                }
+
+            }
+        }
+
+        private void enterGame() {
+            int cx = Screen.PrimaryScreen.Bounds.Width / 2;
+            int cy = Screen.PrimaryScreen.Bounds.Height * 1320 / 1440;
+            SetCursorPos(cx, cy);
+            mouse_event(0x0002, 0, 0, 0, 0); 
+            Thread.Sleep(55);
+            mouse_event(0x0004, 0, 0, 0, 0); 
+            Thread.CurrentThread.Abort();
+        }
+
+        private bool activateWindow(string title) {
+            IntPtr findPtr = FindWindow(null, title);
+
+            if (findPtr.ToInt32() != 0) {
+                ShowWindow(findPtr, 9);
+                SetForegroundWindow(findPtr);
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        private void launchWow() {
+            if (this.activateWindow("暴雪战网"))
+            {
+                Thread.Sleep(300);
+                SendKeys.SendWait("{ENTER}");
+            }
+            else {
+                Console.WriteLine("BN NOT FOUND");
+            }
+            Thread.CurrentThread.Abort();
+        }
+
+        private bool[] checkProcess() {
+            bool[] ret = { false, false };
+            Process[] ps = Process.GetProcesses();
+            foreach (Process p in ps) {
+                if (p.ProcessName.ToLower() == "battle.net") {
+                    ret[0] = true;
+                }
+                if (p.ProcessName.ToLower() == "wow")
+                {
+                    ret[1] = true;
+                }
+            }
+                return ret;
+        }
+
+
+
+        private void appendLog(string log) {
+            List<string> tmp = this.textBox1.Lines.ToList();
+            DateTime dt = DateTime.Now;
+            tmp.Add(string.Format("{0:T}", dt) + " " + log);
+            this.textBox1.Lines = tmp.ToArray();
+        }
+
+        private void toggleUI(bool running) {
+            this.button1.Enabled = !running;
+            this.button2.Enabled = running;
+            this.radioButton1.Enabled = !running;
+            this.radioButton2.Enabled = !running;
+            this.numericUpDown1.Enabled = !running;
+            this.numericUpDown2.Enabled = !running;
+        }
+        private void startService() {
+            this.toggleUI(true);
+            this.enableAutoRestart = this.radioButton1.Checked;
+            this.tickTimer.Enabled = true;
+            appendLog("启用服务");
+        }
+        private void stopService() {
+            this.toggleUI(false);
+            this.tickTimer.Enabled = false;
+            appendLog("停止服务");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool[] psStatus = this.checkProcess();
+            if (!psStatus[0])
+            {
+                MessageBox.Show("请先运行战网客户端！");
+                return;
+            }
+            this.startService();          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.stopService();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("使用说明\n\n1. 保持战网开启并激活魔兽世界子页面\n2. 魔兽世界配置为窗口（最大化）模式\n\nBY:小脑斧\nGithub:Nihiue");
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
